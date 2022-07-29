353675f7440ff09fc3d2f7b0b9fd1b987a5bd2c0
isadfrn <isadfrn@gmail.com>
2022-07-29 00:26:57 -0300

    feat: add update and complete task functions

--- 2022-07-28/todo/src/App.js
+++ 2022-07-28/todo/src/App.js
@@ -5,9 +5,9 @@ import TodoItem from './components/TodoItem';
 
 function App() {
   const [todoItem, setTodoItem] = useState([
-    { todo: 'Monitoro o aquário', complete: true },
-    { todo: 'Dar ração para os gatos', complete: false },
-    { todo: 'Beber remédio', complete: false },
+    { todo: 'Estudar', complete: false },
+    { todo: 'Beber 2 litros de água', complete: false },
+    { todo: 'Entregar trabalho de programação', complete: true },
   ]);
 
   const createTodoItem = (todo) => {
@@ -21,12 +21,42 @@ function App() {
     setTodoItem(newTodoItems);
   };
 
+  const completeTodoItem = (index) => {
+    const newTodoItems = [...todoItem];
+    newTodoItems[index].complete === false
+      ? (newTodoItems[index].complete = true)
+      : (newTodoItems[index].complete = false);
+    setTodoItem(newTodoItems);
+  };
+
+  const updateTodoItem = (index) => {
+    const newTodoItems = [...todoItem];
+    const item = newTodoItems[index];
+
+    let newItem = prompt(`Atualizar item? ${item.todo}`, item.todo);
+
+    if (newItem === null || newItem === '') {
+      return;
+    } else {
+      item.todo = newItem;
+    }
+
+    setTodoItem(newTodoItems);
+  };
+
   return (
     <div className="app">
       <TodoInput createTodoItem={createTodoItem} />
 
       {todoItem.map((item, index) => (
-        <TodoItem key={index} index={index} item={item} deleteTodoItem={deleteTodoItem} />
+        <TodoItem
+          key={index}
+          index={index}
+          item={item}
+          deleteTodoItem={deleteTodoItem}
+          completeTodoItem={completeTodoItem}
+          updateTodoItem={updateTodoItem}
+        />
       ))}
     </div>
   );
--- 2022-07-28/todo/src/components/TodoItem.js
+++ 2022-07-28/todo/src/components/TodoItem.js
@@ -1,7 +1,12 @@
-const TodoItem = ({ index, item }) => {
+const TodoItem = ({ index, item, deleteTodoItem, completeTodoItem, updateTodoItem }) => {
   return (
     <div className="todo-list">
-      <li>{item.todo}</li>
+      <li style={{ textDecoration: item.complete === true ? 'line-through' : '' }}>{item.todo}</li>
+      <div className="btns">
+        <button onClick={() => deleteTodoItem(index)}>✖️</button>
+        <button onClick={() => completeTodoItem(index)}>✔️</button>
+        <button onClick={() => updateTodoItem(index)}>➖</button>
+      </div>
     </div>
   );
 };
